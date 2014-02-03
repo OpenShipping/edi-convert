14b7764ad01b0695a6f0167c9523b6751b995ba7
Kim Hansen <kim@ange.dk>
2014-02-03 17:26:01 +0100

    Fix the vessel parse test
    The test is not a real test that parses a vessel and tests the output.
    There is room for testing the output a lot more.

--- code/vessel-parser/src/test/java/dk/ange/stowbase/parse/vessel/TestParserVessel.java
+++ code/vessel-parser/src/test/java/dk/ange/stowbase/parse/vessel/TestParserVessel.java
@@ -3,13 +3,9 @@ package dk.ange.stowbase.parse.vessel;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
-import java.io.FileOutputStream;
 import java.io.InputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
 
 import org.junit.Test;
-import org.junit.Ignore;
 
 import dk.ange.stowbase.parse.vessel.ParseVessel.Result;
 
@@ -23,43 +19,18 @@ public class TestParserVessel {
      *
      * @throws Exception
      */
-    @Ignore("This is not really a unit test, it is a program for converting vessels")
     @Test
-    public void convert() throws Exception {
-        //doConvert("vessel-9071533.xls"); // Wuxi Dragon
-        //doConvert("vessel-9071533-wud.xls");
-        //doConvert("vessel-8913679CAD.xls");
-        //doConvert("vessel-1000001.xls");
-        //doConvert("vessel-9124366.xls");
-        //doConvert("vessel-9137894.xls");
-        //doConvert ("vessel-9071533_test.xls");
-        //doConvert("vessel-9071533_test.xls");
-        //doConvert("vessel-9367803-Taicang-Dragon_test.xls");
-    	doConvert("Emma Maersk.xls");
-    }
-
-    private void doConvert(final String resourceName) throws Exception {
-        final InputStream inputStream = TestParserVessel.class.getResourceAsStream(resourceName);
-        final Result result = ParseVessel.parse(inputStream);
-        if (result.messages.getException() != null) {
-            throw new RuntimeException(result.messages.getException());
+    public void convertLegoWithStability() throws Exception {
+        final Result result;
+        try (final InputStream inputStream = TestParserVessel.class.getResourceAsStream("lego-maersk-w-stability.xls")) {
+            result = ParseVessel.parse(inputStream);
         }
-        String x = result.json;
-      FileOutputStream f = new FileOutputStream( resourceName+ ".json");
-//      FileOutputStream f = new FileOutputStream("vessel-9071533-wud.json");
-      Writer w = new OutputStreamWriter(f);
-      w.write(x);
-      w.close();
-      f.close();
-
 
         assertNotNull("Be able to display stack trace", result.messages.getDeveloperStatus());
-        assertNull("Should not get a stack trace:\n" + result.messages.getDeveloperStatus(), result.messages
-                .getException());
+        assertNull("Should not get a stack trace:\n" + result.messages.getDeveloperStatus(),
+                result.messages.getException());
         assertNotNull(result.json);
         assertNotNull(result.messages.getStatus());
-  //      System.out.println(result.messages.getStatus());
-        // org.junit.Assert.assertEquals("For developing", result.messages.getStatus());
     }
 
 }
